89ec9e60463ca5f931362426e67f4c1884698cf2
Harvind Samra <harvind@rangenetworks.com>
2015-01-21 21:32:33 -0800

    Fix for 64-bit OS.

--- UMTS/IntegrityProtect.cpp
+++ UMTS/IntegrityProtect.cpp
@@ -26,14 +26,14 @@
 
 typedef unsigned char u8;
 typedef unsigned short u16;
-typedef unsigned long u32;
+typedef unsigned int u32;
 
 /*---------------------------------------------------------
  *                   Kasumi.h
  *---------------------------------------------------------*/
 typedef unsigned char     u8;
 typedef unsigned short   u16;
-typedef unsigned long    u32;
+typedef unsigned int    u32;
 //void KeySchedule( u8 *key );
 //void Kasumi( u8 *data );
 
